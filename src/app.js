110ae80789f68d442cbab4b2d1787c112239f950
K0ST4N <kostantinatanasov11@gmail.com>
2025-07-06 13:08:36 +0000

    Carta Poker

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+    const suits = ['♥', '♦', '♠', '♣']
+    const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    // Elige aleatoriamente
+  function generateCard (){
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)]
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+
+    // Inserta en el HTML
+    document.getElementById('top-suit').innerText = randomSuit
+    document.getElementById('card-number').innerText = randomNumber
+    document.getElementById('bottom-suit').innerText = randomSuit
+
+    // Cambia color a rojo si es corazón o diamante
+    if (randomSuit === '♥' || randomSuit === '♦') {
+      document.getElementById('top-suit').classList.add('text-danger')
+      document.getElementById('bottom-suit').classList.add('text-danger')
+    } else {
+      document.getElementById('top-suit').classList.add('text-dark')
+      document.getElementById('bottom-suit').classList.add('text-dark')
+    }
+  }
+
+    generateCard()
+
+    // Boton para generar carta
+     document.getElementById('new-card-btn').addEventListener('click', generateCard)
+
+    // Temporizador para que se genere una carta automaticamente 
+     setInterval(generateCard, 10000);
+
+    // cambia el ancho y el alto de la carta 
+      document.getElementById('sizeBtn').addEventListener('click', () => {
+      const width = document.getElementById('width-input').value;
+      const height = document.getElementById('height-input').value;
+
+      const card = document.getElementById('card');
+
+      if (width) {
+        card.style.width = `${width}px`;
+      }
+      if (height) {
+        card.style.height = `${height}px`;
+      }
+    });
